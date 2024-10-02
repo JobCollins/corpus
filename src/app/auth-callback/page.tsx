99a1b200809f65991870777933d5876a8c9e99d4
JobCollins <dulo.collins@gmail.com>
2024-10-02 13:18:53 +0300

    Chore: Local Code to Remote

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { useRouter, useSearchParams } from "next/navigation"
+import { trpc } from "../_trpc/client"
+import { Loader2 } from "lucide-react"
+import { useEffect, useState } from "react"
+
+const Page = () => {
+    const router = useRouter()
+
+    const searchParams = useSearchParams()
+    const origin = searchParams.get('origin')
+
+    const { data, isLoading, error } = trpc.authCallBack.useQuery(undefined, {
+        retry: true,
+        retryDelay: 500,
+    })
+
+    const [redirected, setRedirected] = useState(false);
+
+    useEffect(() => {
+        if (data?.success) {
+                // user is synced to db
+                console.log('Data fetched successfully:', data);
+                setRedirected(true);  // Ensure redirect happens only once
+                router.push(origin ? `/${origin}` : '/dashboard')
+            
+        }
+        else if (error && error.data?.code === 'UNAUTHORIZED') {
+            setRedirected(true);  // Prevent re-execution
+            router.push('/sign-in')
+        }
+
+    }, [data, origin, router, error]);
+    return (
+        <div className="w-full mt-24 flex justify-center">
+            <div className="flex flex-col items-center gap-2">
+                <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+                <h3 className="font-semibold text-xl">Setting up your account...</h3>
+                <p>You will be redirected soon.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Page
