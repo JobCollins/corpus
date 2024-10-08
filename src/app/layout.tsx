92430baea6d9343c6107d801e67f0f94465c06af
JobCollins <dulo.collins@gmail.com>
2024-10-08 13:01:31 +0300

    Feature: Control Bar for PDF Renderer
    zoom, pagination, rotation, and fullscreen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,9 @@ import NavBar from "@/components/ui/NavBar";
 import Providers from "@/components/Providers";
 
 import "react-loading-skeleton/dist/skeleton.css"
+
 import { Toaster } from "@/components/ui/toaster";
+import "simplebar-react/dist/simplebar.min.css"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
