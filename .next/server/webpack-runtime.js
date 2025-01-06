bf2806b7737644f01a7b78cf056b7862361d1cbc
JobCollins <dulo.collins@gmail.com>
2025-01-06 11:00:30 +0300

    Feature: Payment
    stripe payment and billing form configuration

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9e5c6750087abb5b")
+/******/ 		__webpack_require__.h = () => ("00af1ac1ffb1861b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b5ab24e43ab367d"; }
+/******/ 		__webpack_require__.h = function() { return "72e594956e9227bd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
