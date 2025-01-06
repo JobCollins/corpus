a91a6cd3e51cedb6d204a7549a01055570865a24
JobCollins <dulo.collins@gmail.com>
2025-01-06 13:37:04 +0300

    BugFix: Vercel Build Fail
    corrected stripe apiversion

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("325e3b9b45fb879b")
+/******/ 		__webpack_require__.h = () => ("971d36a275bcabf0")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
